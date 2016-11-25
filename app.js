83a6566bd1eabb0070a22b24e5841406631660c6
Harihar Subramanyam <hahasubbu@gmail.com>
2016-11-25 09:53:41 -0500

    Merge bin/www and app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
+
+var argv = require('yargs').argv;
 
 var app = express();
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var server = http.createServer(app);
+
 const PacketCounter = require("./network/counter");
 const PacketSniffer = require("./network/sniffer");
 
@@ -60,12 +70,12 @@ const setupSocketIo = http => {
   const sniffer = new PacketSniffer();
 
   // Launch the packet sniffer. It will send its packets to the packet 
-  // counter above. If a filepath has been given (process.argv[2]), then we
+  // counter above. If a filepath has been given (argv.path), then we
   // will read from that pcap file. Otherwise, we will start sniffing actual
   // packets.
   sniffer.sniff(
     counter,
-    (process.argv.length >= 3) ? process.argv[2] : undefined
+    argv.path
   );
 
   // This is not really useful right now, but it will later allows us to
@@ -82,4 +92,55 @@ const setupSocketIo = http => {
 
 };
 
-module.exports = {app, setupSocketIo};
+setupSocketIo(server);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string' ? 
+  'Pipe ' + port : 
+  'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 
+    'pipe ' + addr : 
+    'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
