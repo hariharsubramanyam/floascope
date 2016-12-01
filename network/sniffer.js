3a1073e48cfd05d7125b0c770513b6f9fe689367
Harihar Subramanyam <hahasubbu@gmail.com>
2016-12-01 15:00:41 -0500

    Comments

--- network/sniffer.js
+++ network/sniffer.js
@@ -4,23 +4,31 @@
 // This is the packet sniffing library.
 const pcap = require("pcap");
 
+// When reading packets from a file, we can't play them back all at once, we 
+// need to delay them according to their timestamps. This contains the logic
+// for delaying packets.
 const PacketDelayer = require("./delayer");
+
+// This is a custom version of node_pcap's TCPTracker, which accepts sniffed
+// packets and computes useful statistics on TCP sessions.
 const TCPTracker = require("./tcp_tracker").TCPTracker;
 
 /**
  * This is the packet sniffer. It can either sniff actual packets or sniff
  * packets from a .pcap file.
  *
- * Currently, it only sniffs TCP sessions.
+ * Currently, it only works for TCP sessions.
  */
 class PacketSniffer {
   constructor() {
-    // This will clean raw packets and extract info about TCP sessions.
     this.tcp_tracker = new TCPTracker();
   }
 
   /**
-   * Start sniffing for packets.
+   * Start sniffing for packets. At a high level, we use the following flow:
+   *
+   * packer => delayer => TCP tracker => counter => broadcast to clients
+   *
    * @param {PacketCounter} counter - This will receive information from the
    *  Sniffer.
    * @param {Number} ffwd - The fast-forward ratio. If this is set to 2.0, for
@@ -45,32 +53,40 @@ class PacketSniffer {
       session.on("data recv", session => counter.updateSession(session));
       session.on("data send", session => counter.updateSession(session));
 
+      // On retransmit, notify that the counter that a retransmissing (of
+      // the given byte length) has occured for the given session.
       session.on("retransmit", (session, sendOrRecv, endOfData, len) => {
-        // On retransmit, update the number of retransmissions.
         counter.retransmit(session, len);
       });
 
+      // Purge the session when it closes.
       session.on("end", session => {
-        // Purge the session when it closes.
         counter.removeSession(session);
         console.log("End of session between: " + session.src_name + " and " +
           session.dst_name);
       });
     });
 
-    // Every time we read a packet, decode it and feed it to the TCP tracker.
+    // We need to set up a packet delayer that we send our packets to after
+    // we sniff them. For live packet sniffing, the delay will simply let the 
+    // pass through right away. For playing back packets from a file, the 
+    // delayer will examine the timestamp and desired playback rate (ffwd) and
+    // will decide when to actually let the packet pass through.
     const tracker = this.tcp_tracker;
     const delayer = new PacketDelayer(
-      path ? false : true,
-      ffwd,
-      packet => {
+      path ? false : true, // Whether the delayer should actually delay packets.
+      ffwd, // The playback rate.
+      packet => { // The callback to execute when the delayer emits a packet.
         try {
+          // We just feed our packet to the TCP Tracker.
           tracker.track_packet(packet);
         } catch(err) {
           console.log(err);
         }
       }
     );
+
+    // Whenever we get a new packet, feed it to the delayer.
     pcap_session.on("packet", raw_packet => delayer.add(raw_packet));
   }
 }
