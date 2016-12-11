f96c17124dd4101f71e028b785381f7845c685a5
Harihar Subramanyam <hahasubbu@gmail.com>
2016-12-11 15:50:10 -0500

    ItemBuffer to store server events

---
+++ public/js/buffer.js
@@ -0,0 +1,23 @@
+(function() {
+  ItemBuffer = function(maxLength) {
+    maxLength = maxLength || 10000;
+    var buffer = [];
+
+    var that = Object.create(ItemBuffer.prototype);
+
+    that.buffer = function(item) {
+      buffer.push(item);
+      while (buffer.length > maxLength) {
+        buffer.shift();
+      }
+    };
+
+    that.replay = function(fn) {
+      buffer.forEach(fn);
+      buffer = [];
+    };
+
+    Object.freeze(that);
+    return that;
+  };
+})();
